4cad7390dafa9b80a8932c904ea0d96ee35ebf43
Dmitry Zavodnikov <dzavodnikov@sitime.com>
2022-02-16 20:19:15 +0100

    Add example of histogram calculation.

---
+++ src/stat_examples.py
@@ -0,0 +1,11 @@
+from scipy import ndimage
+
+
+d1 = [1, 1, 5, 6, 9]
+h1 = ndimage.histogram(d1, 1, 10, 5)
+print(h1)
+
+# Out of (min, max) are ignired.
+d2 = [-1, 1, 1, 5, 6, 9, 11]
+h2 = ndimage.histogram(d2, 1, 10, 5)
+print(h2)
